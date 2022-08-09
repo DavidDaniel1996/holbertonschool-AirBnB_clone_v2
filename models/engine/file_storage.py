d8f4a2adbcdc4a3a0950875555bf694e9341a4b6
David <4292@holbertonschool.com>
2022-08-09 13:54:46 -0400

    adding delete and specify class in all functionality

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        class_dict = {}
+        if cls is not None:
+            for k, v in FileStorage.__objects.items():
+                if v.__class__ == cls:
+                    class_dict[k] = v
+            return class_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +51,11 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        del FileStorage.__objects[f"{type(obj).__name__}.{obj.id}"]
